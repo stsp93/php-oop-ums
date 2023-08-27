d13a4d1007039839f5a9dd22d66aa35033b9d5a2
stsp93 <stspsp@gmail.com>
2023-08-27 13:23:09 +0300

    added db class

--- config/Database.php
+++ config/Database.php
@@ -1,5 +1,6 @@
-<?php 
-class Database {
+<?php
+class Database
+{
     private $host = 'localhost';
     private $db_name = 'task7';
     private $username = 'username';
@@ -13,21 +14,16 @@ class Database {
         try {
             $this->pdo = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             $this->error = $e->getMessage();
             echo $this->error;
         }
     }
 
-    public function query($sql, $params = []) {
-        try {
-            $this->stmt = $this->pdo->prepare($sql);
-            $this->stmt->execute($params);
-            return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-        }catch(PDOException $e) {
-            $this->error = $e->getMessage();
-            echo $this->error;
-        }
+    public function query($sql, ...$params)
+    {
+        $this->stmt = $this->pdo->prepare($sql);
+        $this->stmt->execute([...$params]);
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
 }
